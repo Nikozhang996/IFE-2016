01e38b667075f2b1bb6427fd03a44c116c219c02
Niko_Zhang <593177876@qq.com>
2018-05-15 15:26:31 +0800

    完善了PageSwitch插件

--- 日常训练/全屏滚动切换插件/PageSwitch-first.js
+++ 日常训练/全屏滚动切换插件/PageSwitch-first.js
@@ -1,95 +1,113 @@
 (function ($) {
-  /* 添加浏览器前缀 */
-  var _prefix = (function (temp) {
+  /* 
+  说明：添加浏览器前缀
+  实现：判断某个元素的css样式中是否存在transition属性
+  参数：dom元素
+  返回值：boolean，有则返回浏览器样式前缀，否则返回false
+  */
+  var _prefix = (function (element) {
     var aPrefix = ["webkit", "Moz", "o", "ms"];
     var props = "";
     for (var i in aPrefix) {
       props = aPrefix[i] + "Transition";
-      if (temp.style[props] !== undefined) {
+      if (element.style[props] !== undefined) {
         return "-" + aPrefix[i].toLowerCase() + "-";
       }
     }
     return false;
   })(document.createElement(PageSwitch));
 
-  /*  */
+  /*  
+  说明：通过闭包实现单例模式返回PageSwitch插件
+  返回值：PageSwitch构造函数
+  */
   var PageSwitch = (function () {
     function PageSwitch(element, options) {
+      // 通过内置类获取插件
       this.settings = $.extend(true, $.fn.PageSwitch.defaults, options || {});
+
+      // 当前容器元素
       this.element = element;
+
+      // 初始化插件
       this.init();
     }
     PageSwitch.prototype = {
       construstor: PageSwitch,
       /* 初始化插件 */
       init: function () {
-        var self = this;
-        self.selectors = self.settings.selectors;
-        self.sections = self.element.find(self.selectors.sections);
-        self.section = self.sections.find(self.selectors.section);
+        //  获取选择器，区块大容器，每个区块，显示方向，页数
+        this.selectors = this.settings.selectors;
+        this.sections = this.element.find(this.selectors.sections);
+        this.section = this.sections.find(this.selectors.section);
+        this.direction = this.settings.direction == "vertical" ? true : false;
+        this.pagesCount = this.pagesCount();
 
-        self.direction = self.settings.direction == "vertical" ? true : false;
-        self.pagesCount = self.pagesCount();
-
-        self.index = (self.settings.index >= 0 && self.settings.index < self.pagesCount) ?
-          self.settings.index :
+        // 0 < index < 页数，否则重置为0
+        this.index = (this.settings.index >= 0 && this.settings.index < this.pagesCount) ?
+          this.settings.index :
           0;
 
-        self.canScroll = true;
+        // 判断动画是否正在进行
+        this.isScrolling = true;
 
-        if (!self.direction) {
-          self._initLayout();
+        if (!this.direction) {
+          this._initLayout();
+        }
+        if (this.settings.pagination) {
+          this._initPaging();
         }
 
-        if (self.settings.pagination) {
-          self._initPaging();
-        }
-
+        // 初始化所有事件
         this._initEvent();
       },
 
-      /* 获取滑动页面数量 */
+      /*说明： 获取滑动页面数量 */
       pagesCount: function () {
         return this.section.length;
       },
 
-      /* 获取滑动的宽度或高度 */
+      /* 说明：获取滑动的宽度或高度 */
       switchLength: function () {
+        // 如果是vertical则获取元素高度，如果是horizontal则获取元素宽度
         return this.direction == 1 ? this.element.height() : this.element.width();
       },
 
-      //说明：向前滑动即上一页
+      /* 说明：向前滑动即上一页 */
       prve: function () {
-        var self = this;
-        if (self.index > 0) {
-          self.index--;
-        } else if (self.settings.loop) {
-          self.index = self.pagesCount - 1;
+        // 如果loop:true，则重置index
+        if (this.index > 0) {
+          this.index--;
+        } else if (this.settings.loop) {
+          this.index = this.pagesCount - 1;
         }
-        self._scrollPage();
+        this._scrollPage();
       },
 
-      //说明：向后滑动即下一页
+      /* 说明：向后滑动即下一页 */
       next: function () {
-        var self = this;
-        if (self.index < self.pagesCount) {
-          self.index++;
-        } else if (self.settings.loop) {
-          self.index = 0;
+        if (this.index < this.pagesCount) {
+          this.index++;
+        } else if (this.settings.loop) {
+          this.index = 0;
         }
-        self._scrollPage();
+        this._scrollPage();
       },
 
 
-      /* 针对横屏情况进行页面布局 */
+      /* 说明： 针对横屏情况进行页面布局 */
       _initLayout: function () {
         var self = this;
+
+        // 计算父容器总宽度，子容器各占的百分比。
         if (!this.direction) {
           var width = (this.pagesCount * 100) + "%",
             cellWidth = (100 / this.pagesCount).toFixed(2) + "%";
           this.sections.width(width);
           this.section.width(cellWidth).css("float", "left");
         }
+
+        // 如果index存在，允许滚动
         if (this.index) {
           this._scrollPage(true);
         }
@@ -97,9 +115,11 @@
 
       /* 实现分页的DOM结构和CSS样式 */
       _initPaging: function () {
+        // 提取Class
         var pagesClass = this.selectors.page.substring(1);
         this.activeClass = this.selectors.active.substring(1);
 
+        // 生成分页DOM结构
         var pageHtml = 'ul class=' + pagesClass + '>';
         for (var i = 0; i < this.pagesCount; i++) {
           pageHtml += '<li></li>';
@@ -114,7 +134,7 @@
 
         if (this.direction) {
           pages.addClass('vertical');
-        } else if (this.settings.pagination) {
+        } else {
           pages.addClass('horizontal');
         }
       },
@@ -125,15 +145,16 @@
 
         /* 绑定鼠标点击切换事件 */
         this.element.on('click', self.selectors.pages + 'li', function (ev) {
+          console.log(this);
+
           self.index = $(this).index();
           self._scrollPage();
         });
 
         /* 绑定鼠标滚轮事件 */
         this.element.on('mousewheel DOMMouseScroll', function (ev) {
-          if (self.canScroll) {
+          if (self.isScrolling) {
             var delta = ev.originalEvent.wheelDelta || -ev.originalEvent.detail;
-
             if (delta > 0 && (self.index && !self.settings.loop || self.settings.loop)) {
               self.prve();
             } else if (delta < 0 && (self.index < (self.pagesCount - 1) && !self.settings.loop || self.settings
@@ -158,25 +179,27 @@
         /* 绑定窗口改变事件 */
         var resizeId;
         $(window).resize(function () {
-          clearTiselfout(resizeId);
-          resizeId = setTiselfout(function () {
+          clearTimeout(resizeId);
+          resizeId = setTimeout(function () {
             var currentLength = self.switchLength();
-            var offset = self.settings.direction ? self.section.eq(self.index).offset().top : self.section
-              .eq(
-                self.index).offset().left;
+
+            // 获取当前窗口的位移量
+            var offset = self.direction ? self.section.eq(self.index).offset().top : self.section.eq(self.index).offset().left;
+
+            // 判断偏移量是否大于当前屏幕的一半
             if (Math.abs(offset) > currentLength / 2 && self.index < (self.pagesCount - 1)) {
               self.index++;
             }
             if (self.index) {
               self._scrollPage();
             }
-          }, 500);
+          }, 100)
         });
 
-        //支持CSS3动画的浏览器，绑定transitionend事件(即在动画结束后调用起回调函数)
+        /* 绑定transitionend事件(即在动画结束后调用起回调函数) */
         if (_prefix) {
           self.sections.on("transitionend webkitTransitionEnd oTransitionEnd otransitionend", function () {
-            self.canscroll = true;
+            self.isScrolling = true;
             if (self.settings.callback && $.type(self.settings.callback) === "function") {
               self.settings.callback();
             }
@@ -186,13 +209,18 @@
 
       /* 滑动动画 */
       _scrollPage: function (init) {
+        // dest为当前元素偏移量
         var dest = this.section.eq(this.index).position();
         if (!dest) return;
 
-        this.canscroll = false;
+        this.isScrolling = false;
+
+        console.log(dest);
+        
+        // 如果支持css3动画则使用transition，否则用animate动画
         if (_prefix) {
-          //var translate = this.direction ? "translateY(-" + dest.top + "px)" : "translateX(-" + dest.left + "px)";
-          var translate = this.direction ? `translateY(-${dest.top}px)` : `translateX(-${dest.left}px)`;
+          var translate = this.direction ? "translateY(-" + dest.top + "px)" : "translateX(-" + dest.left + "px)";
+          // var translate = this.direction ? `translateY(-${dest.top}px)` : `translateX(-${dest.left}px)`;
           this.sections.css(_prefix + "transition", "all " + this.settings.duration + "ms " + this.settings.easing);
           this.sections.css(_prefix + "transform", translate);
         } else {
@@ -202,7 +230,7 @@
             left: -dest.left
           };
           this.sections.animate(animateCss, this.settings.duration, function () {
-            this.canscroll = true;
+            this.isScrolling = true;
             if (this.settings.callback) {
               this.settings.callback();
             }
@@ -217,6 +245,8 @@
   })();
 
   $.fn.PageSwitch = function (options) {
+    console.log(this);
+    
     return this.each(function () {
       var self = $(this),
         instance = self.data("PageSwitch");
