0c1c875964dbb15f6e45025a1b42c77be83abc51
Niko_Zhang <593177876@qq.com>
2018-08-05 21:29:34 +0800

    优化了生成日期函数

--- 日常训练/生成日期.js
+++ 日常训练/生成日期.js
@@ -1,14 +1,23 @@
+/* 
+
+ */
 function createTimeList(start, end, step) {
     let flag = ':';
-    let startMin = start.split(flag)[0],
-        startSec = start.split(flag)[1],
-        endMin = end.split(flag)[0],
-        endSec = end.split(flag)[1],
-        stepMin = step.split(flag)[0],
-        stepSec = step.split(flag)[1];
+    let [startMin, startSec] = start.split(flag);
+    let [endMin, endSec] = end.split(flag);
+
+    let [stepMin, stepSec] = step.split(flag);
 
     let minTemp = parseInt(startMin, 10),
         secTemp = parseInt(startSec, 10);
+
+    if (endMin == '00' || '0') {
+        endMin = '24';
+    }
+    if (startMin > endMin) {
+        throw new Error('结束时间必须大于开始时间');
+        return     
+    }
 
     let tempArr = [];
 
@@ -26,3 +35,5 @@ function createTimeList(start, end, step) {
     tempArr.push(`${(endMin + '').padStart(2, '0')}:${(endSec + '').padStart(2, '0')}`);
     return tempArr;
 }
+
+console.log(createTimeList('08:00', '01:00', '01:00'));
